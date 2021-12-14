4f0c4f8f7a2a631e7968f4e07515029e841327bd
onyin1 <92761606+onyin1@users.noreply.github.com>
2021-12-14 02:10:32 -0500

    new favourites table + insert and query methods

--- app/src/main/java/com/cnit355/rr/DBHelper.java
+++ app/src/main/java/com/cnit355/rr/DBHelper.java
@@ -6,6 +6,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.util.ArrayList;
+
 //Class to handle database functions
 
 public class DBHelper extends SQLiteOpenHelper {
@@ -18,11 +20,14 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase DB) {
         DB.execSQL("create Table users(username TEXT primary key, password TEXT)"); //Create table users with primary key username and password field
+        DB.execSQL("create Table favourites(username TEXT, word TEXT, FOREIGN KEY (username)\n" +
+                "    REFERENCES users (username), primary key (username, word) )");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase DB, int i, int i1) {
         DB.execSQL("drop Table if exists users");
+        DB.execSQL("drop Table if exists favourites");
     }
 
     public Boolean insertData(String username, String password){
@@ -53,10 +58,67 @@ public class DBHelper extends SQLiteOpenHelper {
         SQLiteDatabase LoginDB = this.getWritableDatabase();
 
         //check password of user in table using cursor to go through rows
-        Cursor cursor = LoginDB.rawQuery("Select * from users where username = ? and password = ?", new String[] {username,password});
+        Cursor cursor = LoginDB.rawQuery("Select * from users where username = ? and password = ? ", new String[] {username,password});
+
         if(cursor.getCount()>0)
             return true;
         else
             return false;
+    }
+
+    //adding a user's favourite words to the database
+
+    public boolean insertFavouriteWord(String username, String word) {
+
+        SQLiteDatabase LoginDB = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        // on below line we are passing all values
+        // along with its key and value pair.
+        values.put("username", username);
+        values.put("word", word);
+
+        // after adding all values we are passing
+        // content values to our table.
+        long result= LoginDB.insert("favourites", null, values);
+
+        if(result==-1) return false;
+        else
+            return true;
+    }
+
+    //to use: create  DBHelper db = new DBHelper(this); as global variable
+    // Boolean insertWord = db.insertFavouriteWord(username, word);
+    //            if(insertWord==true){
+    //                Toast.makeText(this, "Favourite word saved!", Toast.LENGTH_SHORT).show();
+    //            }else{
+    //                Toast.makeText(this, "Could not save favourite word", Toast.LENGTH_SHORT).show();
+    //            }
+
+
+    //method to see a users favourite words, returns array of the words
+    public ArrayList<String> favouriteWords(String username) {
+
+        SQLiteDatabase LoginDB = this.getReadableDatabase();
+
+        // on below line we are creating a cursor with query to read data from database.
+        Cursor cursor = LoginDB.rawQuery("Select word from favourites where username = ? ", new String[] {username});
+
+        // on below line we are creating a new array list.
+        ArrayList<String> favouriteWords = new ArrayList<>();
+
+        // moving our cursor to first position.
+        if (cursor.moveToFirst()) {
+            do {
+                // on below line we are adding the data from cursor to our array list.
+                favouriteWords.add((cursor.getString(cursor.getColumnIndexOrThrow("word"))));
+
+            } while (cursor.moveToNext());
+            // moving our cursor to next.
+        }
+        // at last closing our cursor
+        // and returning our array list.
+        cursor.close();
+        return favouriteWords;
     }
 }
